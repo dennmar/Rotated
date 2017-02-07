aea22c0740604db643ddf0a7141fba7a0dac058c
dennmar <dennismariano3@gmail.com>
2017-02-06 23:47:55 -0800

    Designed prototype starting menu (with no current functionality)

--- app/src/main/java/com/extracliff/rotated/StartFragment.java
+++ app/src/main/java/com/extracliff/rotated/StartFragment.java
@@ -5,6 +5,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+// TODO: Play will lead to easy, medium, hard game selection
+// TODO: Levels will lead to level selection
+// TODO: Themes will lead to theme selection
+// TODO: Help will lead to tutorial on how to play (and maybe tips for gesture shortcuts)
 
 /**
  * Acts as the start menu and allows the user to start a game, change themes, or get help
@@ -20,7 +26,9 @@ public class StartFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 													 Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.start_frag_view, container, false);
+		final View rootView = inflater.inflate(R.layout.start_frag_view, container, false);
+
+		return rootView;
 	}
 
 	@Override
