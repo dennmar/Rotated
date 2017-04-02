5ad7483843c12c330a711ddf34b43f449c982db3
dennmar <dennismariano3@gmail.com>
2017-04-01 21:30:02 -0700

    Moved rotating functionality into the Square class

--- app/src/main/java/com/extracliff/rotated/EasyGameFragment.java
+++ app/src/main/java/com/extracliff/rotated/EasyGameFragment.java
@@ -1,7 +1,7 @@
 /*
  * File: EasyGameFragment.java
  * Description: Allows the user to play an easy game of Rotated
- * Version: 0.2
+ * Version: 0.21
  * Date: 1/30/17
  */
 
@@ -9,14 +9,14 @@ package com.extracliff.rotated;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import java.util.ArrayList;
-
+/**
+ * Starts an easy game of Rotated.
+ */
 public class EasyGameFragment extends Fragment {
 
 	public static final int MIN_ROW = 1;
@@ -26,7 +26,7 @@ public class EasyGameFragment extends Fragment {
 	public static final int ROW_NUM_INDEX = 1;
 	public static final int COL_NUM_INDEX = 3;
 	public static final int WHITE_LEVEL = 1;
-	public static final int BLACK_LEVEL = 0;
+	public static final int BOARD_SIZE = 5;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -49,8 +49,11 @@ public class EasyGameFragment extends Fragment {
 				currView.setOnClickListener(new View.OnClickListener() {
 					@Override
 					public void onClick(View v) {
-						rotate(currView);
-					  rotateNeighbors(currView, rootView);
+						String cSquareTag = (String)(currView.getTag());
+						int rowNumber = Character.getNumericValue(cSquareTag.charAt(ROW_NUM_INDEX));
+						int colNumber = Character.getNumericValue(cSquareTag.charAt(COL_NUM_INDEX));
+						Square currSquare = new Square(currView, rowNumber, colNumber, rootView, BOARD_SIZE);
+						currSquare.rotateWithNeighbors();
 					}
 				});
 			}
@@ -62,80 +65,5 @@ public class EasyGameFragment extends Fragment {
 	@Override
 	public void onPause() {
 		super.onPause();
-	}
-
-
-	/**
-	 * Rotates the squares directly above, below, to the left of, and to the right of the current
-	 *     square as well as the current square itself.
-	 * @param currentSquare The square that was tapped
-	 * @param rootView The view that includes all views in the fragment
-	 */
-	private void rotateNeighbors(View currentSquare, View rootView) {
-		String cSquareTag = (String)(currentSquare.getTag());
-		Log.d("TAGS", "cSquareTag is " + cSquareTag);
-		int rowNumber = Character.getNumericValue(cSquareTag.charAt(ROW_NUM_INDEX));
-		int colNumber = Character.getNumericValue(cSquareTag.charAt(COL_NUM_INDEX));
-		Log.d("NUMBERS", "rowNumber is " + rowNumber);
-		Log.d("NUMBERS", "colNumber is " + colNumber);
-		ArrayList<ImageView> neighbors = findNeighbors(rowNumber, colNumber, rootView);
-		int i = 0;
-		while (i < neighbors.size()) {
-			ImageView currView = neighbors.get(i);
-			rotate(currView);
-			i++;
-		}
-	}
-
-	/**
-	 * Locates the squares directly above, below, to the left of, and to the right of the square
-	 *     that possesses the given coordinates.
-	 * @param row The row number of the square
-	 * @param col The column number of the square
-	 * @param rootView The view that includes all views in the fragment
-	 * @return All of the neighboring squares
-	 */
-	private ArrayList<ImageView> findNeighbors(int row, int col, View rootView) {
-		ArrayList<ImageView> neighbors = new ArrayList<>();
-		if (row != MAX_ROW) {
-			String belowTag = "r" + (row + 1) + "c" + col;
-			Log.d("TAGS", "belowTag is " + belowTag);
-			ImageView below = (ImageView)rootView.findViewWithTag(belowTag);
-			neighbors.add(below);
-		}
-		if (row != MIN_ROW) {
-			String aboveTag = "r" + (row - 1) + "c" + col;
-			Log.d("TAGS", "aboveTag is " + aboveTag);
-			ImageView above = (ImageView)rootView.findViewWithTag(aboveTag);
-			neighbors.add(above);
-		}
-		if (col != MAX_COL) {
-			String rightTag = "r" + row + "c" + (col + 1);
-			Log.d("TAGS", "rightTag is " + rightTag);
-			ImageView right = (ImageView)rootView.findViewWithTag(rightTag);
-			neighbors.add(right);
-		}
-		if (col != MIN_COL) {
-			String leftTag = "r" + row + "c" + (col - 1);
-			Log.d("TAGS", "leftTag is " + leftTag);
-			ImageView left = (ImageView)rootView.findViewWithTag(leftTag);
-			neighbors.add(left);
-		}
-		return neighbors;
-	}
-
-	/**
-	 * Rotates the given square.
-	 * @param square The square whose color will be switched
-	 */
-	private void rotate(ImageView square) {
-		if (square.getDrawable().getLevel() == WHITE_LEVEL) {
-			square.setImageResource(R.drawable.rounded_square_black_mdpi);
-			square.getDrawable().setLevel(BLACK_LEVEL);
-		}
-		else {
-			square.setImageResource(R.drawable.rounded_square_mdpi);
-			square.getDrawable().setLevel(WHITE_LEVEL);
-		}
 	}
 }
---
+++ app/src/main/java/com/extracliff/rotated/Square.java
@@ -0,0 +1,107 @@
+/*
+ * File: Square.java
+ * Description: A rotatable Square that also rotates its neighbors
+ * Version: 0.1
+ * Date: 4/1/17
+ */
+
+package com.extracliff.rotated;
+
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+/**
+ * Defines a rotatable Square where all neighbors must be rotated if the Square is rotated.
+ */
+public class Square {
+
+	public static final int MIN_ROW = 1;
+	public static final int MIN_COL = 1;
+	public static final int WHITE_LEVEL = 1;
+	public static final int BLACK_LEVEL = 0;
+	private int maxRow;
+	private int maxCol;
+	private ImageView square;
+	private int row;
+  private int col;
+	private View rootView;
+
+	/**
+	 * Initializes the instance variables with the given values.
+	 * @param sq The rotatable square
+	 * @param rowNumber The row that the rotatable square is in
+	 * @param colNumber The column that the rotatable square is in
+	 * @param rView The view that includes all views in the fragment
+	 * @param boardSize The maximum row and column number
+ 	*/
+	public Square(ImageView sq, int rowNumber, int colNumber, View rView, int boardSize) {
+		square = sq;
+		row = rowNumber;
+		col = colNumber;
+		rootView = rView;
+		maxRow = boardSize;
+		maxCol = boardSize;
+	}
+
+	/**
+	 * Rotates the square and its neighbors: the squares above, below, to the left of, and to the
+	 *     right of it.
+ 	 */
+	public void rotateWithNeighbors() {
+		rotate(square);
+		ArrayList<ImageView> neighbors = findNeighbors();
+		int i = 0;
+		while (i < neighbors.size()) {
+			ImageView currSquare = neighbors.get(i);
+			rotate(currSquare);
+			i++;
+		}
+	}
+
+	/**
+	 * Locates the squares directly above, below, to the left of, and to the right of the square
+	 *     that possesses the given coordinates.
+	 * @return All of the neighboring squares
+	 */
+	private ArrayList<ImageView> findNeighbors() {
+		ArrayList<ImageView> neighbors = new ArrayList<>();
+		if (row != maxRow) {
+			String belowTag = "r" + (row + 1) + "c" + col;
+			ImageView below = (ImageView)rootView.findViewWithTag(belowTag);
+			neighbors.add(below);
+		}
+		if (row != MIN_ROW) {
+			String aboveTag = "r" + (row - 1) + "c" + col;
+			ImageView above = (ImageView)rootView.findViewWithTag(aboveTag);
+			neighbors.add(above);
+		}
+		if (col != maxCol) {
+			String rightTag = "r" + row + "c" + (col + 1);
+			ImageView right = (ImageView)rootView.findViewWithTag(rightTag);
+			neighbors.add(right);
+		}
+		if (col != MIN_COL) {
+			String leftTag = "r" + row + "c" + (col - 1);
+			ImageView left = (ImageView)rootView.findViewWithTag(leftTag);
+			neighbors.add(left);
+		}
+		return neighbors;
+	}
+
+	/**
+	 * Rotates the given square.
+	 * @param square The square whose color will be switched
+	 */
+	private void rotate(ImageView square) {
+		if (square.getDrawable().getLevel() == WHITE_LEVEL) {
+			square.setImageResource(R.drawable.rounded_square_black_mdpi);
+			square.getDrawable().setLevel(BLACK_LEVEL);
+		}
+		else {
+			square.setImageResource(R.drawable.rounded_square_mdpi);
+			square.getDrawable().setLevel(WHITE_LEVEL);
+		}
+	}
+}
